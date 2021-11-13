c00dd7dbec085758cc2762b7fb573c9932795346
ShreyaMore17 <shreyamore7@gmail.com>
2021-11-13 08:40:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 setup(name='ib16ipif',
       version='0.1',
-      author='DSSS',
+      author='Shreya More',
       author_email='shreya.r.more@fau.de',
       packages=find_packages(),
       install_requires=['numpy', 'Pillow', 'ipywidgets'])

f548b6889a750740614c3cfc9e52f56ca716276b
ShreyaMore17 <shreyamore7@gmail.com>
2021-11-13 12:10:23 +0100

    Update function.py

--- ib16ipif/function.py
+++ ib16ipif/function.py
@@ -2,9 +2,8 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 
-
 def imshow(X, resize=None):
-    new_im = Image.fromarray(X)
-    if resize:
-        new_im= new_im.resize(resize)
-    return plt.imshow(new_im)
+    image=Image.fromarray(X)
+    if resize!=None:
+        image = image.resize(resize)
+    return image.show()
